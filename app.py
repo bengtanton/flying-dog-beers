6537879ef1ca8804d323f11da749785db59d56c9
antonson <anton.zita@bengtdahlgren.se>
2019-08-26 12:50:50 +0200

    testar om de är kompisar nu

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Set up the chart
+# Testar om det lirar nu
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
